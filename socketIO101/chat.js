c5cd53e2c2140f63effda23b9d79f42aac6e980d
Robert Bunch <rob@digitalcrafts.com>
2018-10-22 18:06:56 -0400

    update 101

--- socketIO101/chat.js
+++ socketIO101/chat.js
@@ -6,13 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
-
-io.of('/admin').on('connection',(socket)=>{
-    console.log("Connection to /admin")
-});
-
 io.on('connection',(socket)=>{
-    console.log("Connection made to main")
     socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
     socket.on('messageToServer',(dataFromClient)=>{
         console.log(dataFromClient)
