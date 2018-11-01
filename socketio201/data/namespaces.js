109ff010e9a988ba51721b5bbad60d2184467158
Robert Bunch <rob@digitalcrafts.com>
2018-11-01 17:00:38 -0400

    Finalize ns

--- socketio201/data/namespaces.js
+++ socketio201/data/namespaces.js
@@ -4,9 +4,9 @@ const Room =  require('../classes/Room');
 
 // Set up the namespaces
 let namespaces = [];
-let wikiNs = new Namespace(1,'Wiki','https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png','/wiki');
-let mozNs = new Namespace(2,'Mozilla','https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png','/mozilla');
-let linuxNs = new Namespace(3,'Linux','https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png','/linux');
+let wikiNs = new Namespace(0,'Wiki','https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png','/wiki');
+let mozNs = new Namespace(1,'Mozilla','https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png','/mozilla');
+let linuxNs = new Namespace(2,'Linux','https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png','/linux');
 
 namespaces.push(wikiNs,mozNs,linuxNs);
 
