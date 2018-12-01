8d8775eb0f4e2049285e3ae232a4de5257588cb6
Robert Bunch <rdbunch@gmail.com>
2018-12-01 10:10:31 -0500

    up

--- agarClone/public/canvasStuff.js
+++ agarClone/public/canvasStuff.js
@@ -11,7 +11,7 @@ function draw(){
     // clear the screen out so the old stuff is gone from the last frame
     context.clearRect(0,0,canvas.width,canvas.height)
 
-    console.log(player.locX, player.locY  )
+    // console.log(player.locX, player.locY  )
 
     // clamp the camera to the player
     const camX = -player.locX + canvas.width/2
