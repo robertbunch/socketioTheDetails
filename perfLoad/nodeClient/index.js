df1a4541fcb75ee55f93499f3dc3f3993980b6e7
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-01-19 19:58:30 -0500

    update starter

--- perfLoad/nodeClient/index.js
+++ perfLoad/nodeClient/index.js
@@ -5,68 +5,76 @@
 // - socket.io-client
 
 const os = require('os');
-const cpus = os.cpus();
-// What do we need to know from node about performance?
-// - CPU load (current)
-// - Memory Useage
-//  - free
-const freeMem = os.freemem();
-//  - total
-const totalMem = os.totalmem();
-const usedMem = totalMem - freeMem;
-const memUseage = Math.floor(usedMem/totalMem*100)/100;
-// - OS type
-const osType = os.type() == 'Darwin' ? 'Mac' : os.type();
-// - uptime
-const upTime = os.uptime();
-// - CPU info
-//  - Type
-const cpuModel = cpus[0].model
-//  - Number of Cores
-const numCores = cpus.length;
-//  - Clock Speed
-const cpuSpeed = cpus[0].speed
 
+function performanceData(){
+    return new Promise((resolve,reject)=>{
+        const cpus = os.cpus();
+        // What do we need to know from node about performance?
+        // - CPU load (current)
+        // - Memory Useage
+        //  - free
+        const freeMem = os.freemem();
+        //  - total
+        const totalMem = os.totalmem();
+        const usedMem = totalMem - freeMem;
+        const memUseage = Math.floor(usedMem/totalMem*100)/100;
+        // - OS type
+        const osType = os.type() == 'Darwin' ? 'Mac' : os.type();
+        // - uptime
+        const upTime = os.uptime();
+        // - CPU info
+        //  - Type
+        const cpuModel = cpus[0].model
+        //  - Number of Cores
+        const numCores = cpus.length;
+        //  - Clock Speed
+        const cpuSpeed = cpus[0].speed
+        getCpuLoad().then((cpuLoadData)=>{
+            resolve({freeMem,totalMem,usedMem,memUseage,osType,upTime,cpuModel,numCores,cpuSpeed,cpuLoadData})
+        });
+    });
+}
 
 // cpus is all cores. we need the average of all the cores which
 // will give us the cpu average
 function cpuAverage(){
+    const cpus = os.cpus();
     // get ms in each mode, BUT this number is since reboot
     // so get it now, and get it in 100ms and compare
-    let totalIdle = 0;
-    let totalTick = 0;
+    let idleMs = 0;
+    let totalMs = 0;
     // loop through each core
-	cpus.forEach((aCore)=>{  
-        for(type in aCore.times) {
-          totalTick += aCore.times[type];
-       }     
-        //Total up the idle time of the core
-        totalIdle += aCore.times.idle;
-      })
-      return {
-		idle: totalIdle / cpus.length,  
-		total: totalTick / cpus.length
-	};
+    cpus.forEach((aCore)=>{
+        // loop through each property of the current core
+        for(type in aCore.times){
+            totalMs += aCore.times[type];
+        }
+        idleMs += aCore.times.idle;
+    });
+    return {
+        idle: idleMs / cpus.length,
+        total: totalMs / cpus.length
+    }
 }
 
-// cpuAverage = gets the average since boot
-// getCpuLoad = get the cpuAverage now, and 100ms from now
-// see how much they have changed, in order to find load
+// because the times property is time since boot, we will get
+// now times, and 100ms from now times. Compare them, that will
+// give us current Load
 function getCpuLoad(){
-    // get first average load
-    const start = cpuAverage();
-    console.log(start,"start")
-    // set a delay of 100ms, to check again
-    setTimeout(()=>{
-        // grab 2nd average load
-        const end = cpuAverage();
-        console.log(end,'end')
-        const idleDif = end.idle - start.idle;
-        const totalDif = end.total - start.total;
-        // calc the %
-        const percCpu = 100 - Math.floor((100 * idleDif / totalDif));
-        // console.log(idleDif / totalDif)
-    },2000)
+    return new Promise((resolve, reject)=>{
+        const start = cpuAverage();
+        setTimeout(()=>{
+            const end = cpuAverage();
+            const idleDifference = end.idle - start.idle;
+            const totalDifference = end.total - start.total;
+            // console.log(idleDifference,totalDifference)
+            // calc the % of used cpu
+            const percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference);
+            resolve(percentageCpu);
+        },100)
+    })
 }
 
-getCpuLoad()
+performanceData().then((data)=>{
+    console.log(data)
+})
