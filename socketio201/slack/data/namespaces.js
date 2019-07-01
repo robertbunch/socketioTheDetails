25a424464204ea7c97a0fc4291d079a8b885c14b
Sovgut Sergey <sergeypba@gmail.com>
2019-07-01 12:06:28 +0300

    Update namespaces.js

--- socketio201/slack/data/namespaces.js
+++ socketio201/slack/data/namespaces.js
@@ -3,12 +3,9 @@ const Namespace =  require('../classes/Namespace');
 const Room =  require('../classes/Room');
 
 // Set up the namespaces
-let namespaces = [];
 let wikiNs = new Namespace(0,'Wiki','https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png','/wiki');
 let mozNs = new Namespace(1,'Mozilla','https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png','/mozilla');
 let linuxNs = new Namespace(2,'Linux','https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png','/linux');
-
-namespaces.push(wikiNs,mozNs,linuxNs);
 
 // Make the main room and add it to rooms. it will ALWAYS be 0
 wikiNs.addRoom(new Room(0,'New Articles','Wiki'));
@@ -25,4 +22,4 @@ linuxNs.addRoom(new Room(1,'Red Hat','Linux'));
 linuxNs.addRoom(new Room(2,'MacOs','Linux'));
 linuxNs.addRoom(new Room(3,'Kernal Development','Linux'));
 
-module.exports = namespaces;
+module.exports = [wikiNs, mozNs, linuxNs];
