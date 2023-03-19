c83de6404df91fb7ef8548233faad775f847735c
Robert Bunch <rdbunch@gmail.com>
2023-03-18 20:38:44 -0400

    Beginning of 35

--- socketio201/slackClone/public/scripts.js
+++ socketio201/slackClone/public/scripts.js
@@ -12,6 +12,25 @@ const socket = io('http://localhost:9000');
 // const socket3 = io('http://localhost:9000/mozilla');
 // const socket4 = io('http://localhost:9000/linux');
 
+//sockets will be put into this array, in the index of their ns.id
+const nameSpaceSockets = [];
+const listeners = {
+    nsChange: [],
+}
+
+const addListeners = (nsId)=>{
+    // nameSpaceSockets[ns.id] = thisNs;
+    if(!listeners.nsChange[nsId]){
+        nameSpaceSockets[nsId].on('nsChange',(data)=>{
+            console.log("Namespace Changed!");
+            console.log(data);
+        })
+        listeners.nsChange[nsId] = true;
+    }else{
+        //nothing to do the listener has been added
+    }
+}
+
 socket.on('connect',()=>{
     console.log("Connected!");
     socket.emit('clientConnect');
@@ -26,13 +45,19 @@ socket.on('nsList',(nsData)=>{
     nsData.forEach(ns=>{
         //update the HTML with each ns
         nameSapcesDiv.innerHTML +=  `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`
-        //join this namespace with io()
-        const thisNs = io(`http://localhost:9000${ns.endpoint}`);
-        thisNs.on('nsChange',(data)=>{
-            console.log("Namespace Changed!");
-            console.log(data);
-        })
+
+        //initialize thisNs as its index in nameSpaceSockets.
+        //If the connection is new, this will be null
+        //If the connection has already been established, it will reconnect and remain in its spot
+        // let thisNs = nameSpaceSockets[ns.id];
         
+        if(!nameSpaceSockets[ns.id]){
+            //There is no socket at this nsId. So make a new connection!
+            //join this namespace with io()
+            nameSpaceSockets[ns.id] = io(`http://localhost:9000${ns.endpoint}`);
+        }
+        addListeners(ns.id);
+
     })
 
     Array.from(document.getElementsByClassName('namespace')).forEach(element=>{
