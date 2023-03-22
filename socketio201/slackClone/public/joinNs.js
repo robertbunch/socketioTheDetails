b62d5f564879e94ac3e0c5a61432faaca93e0c80
Robert Bunch <rdbunch@gmail.com>
2023-03-22 16:24:50 -0400

    Beginning of 37

--- socketio201/slackClone/public/joinNs.js
+++ socketio201/slackClone/public/joinNs.js
@@ -12,13 +12,23 @@ const joinNs = (element,nsData)=>{
     let roomList = document.querySelector('.room-list');
     //clear it out
     roomList.innerHTML = "";
+
+    //init firstRoom var
+    let firstRoom;
+
     //loop through each room, and add it to the DOM
-    rooms.forEach(room=>{
+    rooms.forEach((room,i)=>{
+        if(i === 0){
+            firstRoom = room.roomTitle;
+        }
         console.log(room);
         roomList.innerHTML += `<li class="room" namespaceId=${room.namespaceId}>
             <span class="fa-solid fa-${room.privateRoom ? 'lock' : 'globe'}"></span>${room.roomTitle}
         </li>`
     })
+
+    //init join first room
+    joinRoom(firstRoom,clickedNs.id)
 
     //add click listener to each room so the client can tell the server it wants to join!
     const roomNodes = document.querySelectorAll('.room');
--- socketio201/slackClone/public/joinRoom.js
+++ socketio201/slackClone/public/joinRoom.js
@@ -1,12 +1,19 @@
-const joinRoom = (roomTitle,namespaceId)=>{
+const joinRoom = async(roomTitle,namespaceId)=>{
 
     console.log(roomTitle,namespaceId);
-    nameSpaceSockets[namespaceId].emit('joinRoom',roomTitle,(ackResp)=>{
-        console.log(ackResp);
+    const ackResp = await nameSpaceSockets[namespaceId].emitWithAck('joinRoom',roomTitle);
+    console.log(ackResp);
+    document.querySelector('.curr-room-num-users').innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
+    document.querySelector('.curr-room-text').innerHTML = roomTitle;
 
-        document.querySelector('.curr-room-num-users').innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
-        document.querySelector('.curr-room-text').innerHTML = roomTitle;
 
-    });
+    // nameSpaceSockets[namespaceId].emit('joinRoom',roomTitle,(ackResp)=>{
+    //     console.log(ackResp);
+
+    //     document.querySelector('.curr-room-num-users').innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
+    //     document.querySelector('.curr-room-text').innerHTML = roomTitle;
+
+    // });
+
 
 }
