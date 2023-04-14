8b005a2209275036a7f0de373d3b2c964b9cab24
Robert Bunch <rdbunch@gmail.com>
2023-04-14 15:46:17 -0400

    Wrong collision file

--- starterFiles/agarClone/checkCollisions.js
+++ starterFiles/agarClone/checkCollisions.js
@@ -1,12 +1,12 @@
 const Orb = require('./classes/Orb')
 const io = require('../servers').io;
 
-function checkForOrbCollisions(pData,pConfig, orbs, settings){
+const checkForOrbCollisions = (pData,pConfig, orbs, settings)=>{
     return new Promise((resolve, reject)=>{
         //ORB COLLISIONS
         orbs.forEach((orb,i)=>{
-            // console.log("CHECK FOR COLLISIONS")
         // AABB Test(square)  - Axis-aligned bounding boxes
+            // console.log("CHECK FOR COLLISIONS")
             if(pData.locX + pData.radius + orb.radius > orb.locX 
                 && pData.locX < orb.locX + pData.radius + orb.radius
                 && pData.locY + pData.radius + orb.radius > orb.locY 
@@ -18,15 +18,15 @@ function checkForOrbCollisions(pData,pConfig, orbs, settings){
                     );
                 if(distance < pData.radius + orb.radius){
             //COLLISION!!!
-                    pData.score += 1;
-                    pData.orbsAbsorbed += 1;
+                    pData.score += 1; //incrament score
+                    pData.orbsAbsorbed += 1; //incrament orbs absorbed count
                     // pData.color = orb.color;
                     if(pConfig.zoom > 1){
-                        pConfig.zoom -= .001;
+                        pConfig.zoom -= .001; //update zoom so player doesn't get to big for screen
                     }
-                    pData.radius += 0.25;
+                    pData.radius += 0.25; //increase player size
                     if(pConfig.speed < -0.005){
-                        pConfig.speed += 0.005;
+                        pConfig.speed += 0.005; //increase player speed
                     }else if(pConfig.speed > 0.005){
                         pConfig.speed -= 0.005;
                     }
@@ -44,47 +44,44 @@ function checkForOrbCollisions(pData,pConfig, orbs, settings){
     });
 }
         
-function checkForPlayerCollisions(pData,pConfig,players,playerId){
+const checkForPlayerCollisions = (pData,pConfig,players,playersForUsers,playerId)=>{
     return new Promise((resolve, reject)=>{
         //PLAYER COLLISIONS	
-        players.forEach((curPlayer,i)=>{
-            if(curPlayer.uid != playerId){
-                // console.log(curPlayer.uid,pData.uid)
-                let pLocx = curPlayer.locX
-                let pLocy = curPlayer.locY
-                let pR = curPlayer.radius
+        players.forEach((p,i)=>{
+            if(p.socketId != playerId){
+                // console.log("Checking collision...")
+                let pLocx = p.playerData.locX
+                let pLocy = p.playerData.locY
+                let pR = p.playerData.radius
             // AABB Test - Axis-aligned bounding boxes
                 if(pData.locX + pData.radius + pR > pLocx
                 && pData.locX < pLocx + pData.radius + pR
                 && pData.locY + pData.radius + pR > pLocy 
                 && pData.locY < pLocy + pData.radius + pR){
+                    // console.log("Hit square test!");
             // Pythagoras test
                     distance = Math.sqrt(
                         ((pData.locX - pLocx) * (pData.locX - pLocx)) + 
                         ((pData.locY - pLocy) * (pData.locY - pLocy))	
                         );      
                     if(distance < pData.radius + pR){
-                //COLLISION!!  
+                //COLLISION!!
                         if(pData.radius > pR){
+                            // console.log("Collission!!")
                     // ENEMY DEATH
                             let collisionData = updateScores(pData,curPlayer);
                             if(pConfig.zoom > 1){
                                 pConfig.zoom -= (pR * 0.25) * .001;
                             }
-                            players.splice(i, 1);
-                            resolve(collisionData);
-
+                            players.splice(i, 1); //remove player from server players array
+                            playersForUsers.splice(i,1) //remove player from players array used by clients
+                            resolve(collisionData); //essentially a return statement (because I could't get it work without a promise?)
                         }
-                        // else if(pData.radius < pR){           
-                        //     let collisionData = updateScores(curPlayer,pData);
-                        //     players.forEach((p,i)=>{
-                        //         console.log(players[i].name, i)
-                        //         if (pData.uid == p.uid){
-                        //             players.splice(i, 1);
-                        //         }
-                        //     }); 
-                        //     resolve(collisionData);
-                        // }
+                        
+                        //This code could check to see if it the player who tocked was hit.
+                        //It is commented out since the above code should run on the other player's tock 
+                        //In other words, we only need to consider it a "death" on the attacking players turn
+                        // else if(pData.radius < pR){ }
                     }
                 }
             }
