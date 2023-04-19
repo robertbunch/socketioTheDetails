eaf6cf51f93d91cc25ae56a07b306b36554dd954
Robert Bunch <rdbunch@gmail.com>
2023-04-19 11:19:05 -0400

    Beginning of 61 - end of agarclone

--- agarClone/socketStuff/socketMain.js
+++ agarClone/socketStuff/socketMain.js
@@ -16,12 +16,12 @@ const Orb = require('./classes/Orb');
 //every time one is absorb, the server will make a new one
 const orbs = [];
 const settings = {
-    defaultNumberOfOrbs: 500, //number of orbs on the map
+    defaultNumberOfOrbs: 5000, //number of orbs on the map
     defaultSpeed: 6, //player speed
     defaultSize: 6, //default player speed
     defaultZoom: 1.5, // as the player gets bigger, zoom needs to go out
-    worldWidth: 500,
-    worldHeight: 500,
+    worldWidth: 5000,
+    worldHeight: 5000,
     defaultGenericOrbSize: 5, //smaller than player orbs
 }
 const players = [];
@@ -106,7 +106,19 @@ io.on('connect',(socket)=>{
 
     })
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnect',(reason)=>{
+        // console.log(reason)
+        //loop through players and find the player with THIS players socketId
+        //and splice that player out
+        for(let i = 0; i < players.length; i++){
+            if(players[i].socketId === player.socketId){
+                //these are the droids we're looking for
+                //splice the player out of the players AND playersForUsers
+                players.splice(i,1,{})
+                playersForUsers.splice(i,1,{})
+                break;
+            }
+        }
         //check to see if players is empty. If so, stop "ticking"
         if(players.length === 0){
             clearInterval(tickTockInterval)
