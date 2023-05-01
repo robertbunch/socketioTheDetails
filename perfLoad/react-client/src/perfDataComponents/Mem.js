3342af2b3b837cc7d461e91598c408cf025d8d48
Robert Bunch <rdbunch@gmail.com>
2023-05-01 12:19:46 -0400

    Beginning of 79 - finished mem widget

--- perfLoad/react-client/src/perfDataComponents/Mem.js
+++ perfLoad/react-client/src/perfDataComponents/Mem.js
@@ -1,8 +1,36 @@
+import {useRef} from 'react';
+import drawCircle from '../utilities/canvasLoadAnimation';
 
-const Mem = ()=>{
+const Mem = ({data})=>{
+
+    const {
+        freeMem,
+        memUseage,
+        totalMem,
+        usedMem,
+    } = data
+
+    const memRef = useRef()
+
+    const totalMemInGB = Math.floor(totalMem/1073741824*100)/100
+    const freeMemInGB = Math.floor(freeMem/1073741824*100)/100
+    drawCircle(memRef.current,usedMem*100)
 
     return(
-        <div className="mem col-3">Mem</div>
+        <div className="mem col-3">
+            <h3>Memory Usage</h3>
+            <div className="canvas-wrapper">
+                <canvas ref={memRef} width="200" height="200"></canvas>
+                <div className='mem-text'>{memUseage*100}%</div>
+            </div>
+            <div>
+                Total Memory: {totalMemInGB}gb
+            </div>
+            <div>
+                Free Memory: {freeMemInGB}gb
+            </div>
+
+        </div>
     )
 
 }
