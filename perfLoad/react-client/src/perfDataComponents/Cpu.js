4df7b37f713229d4cebd3b936b5a45cb9905c5a7
Robert Bunch <rdbunch@gmail.com>
2023-05-01 11:00:43 -0400

    Beginning of 78 - done with cpu widget draw

--- perfLoad/react-client/src/perfDataComponents/Cpu.js
+++ perfLoad/react-client/src/perfDataComponents/Cpu.js
@@ -1,8 +1,21 @@
+import {useRef} from 'react';
+import drawCircle from '../utilities/canvasLoadAnimation'
 
-const Cpu = ()=>{
+const Cpu = ({data})=>{
+
+    const canvasEl = useRef();
+    drawCircle(canvasEl.current,data.cpuLoad)
 
     return(
-        <div className="cpu col-3">Cpu</div>
+        <div className="cpu col-3">
+            <h3>CPU Load</h3>
+            <div className="canvas-wrapper">
+                <canvas ref={canvasEl} className="" width="200" height="200"></canvas>
+                <div className="cpu-text">{data.cpuLoad}</div>
+            </div>
+
+
+        </div>
     )
 
 }
